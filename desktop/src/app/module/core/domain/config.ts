0d7dda611f383f7a7195ab4139876f036172b1e5
Fernando Berti <bertilxi@gmail.com>
2017-04-18 13:48:06 -0300

    agregado plugins para facebook y google, falta integrar

---
+++ desktop/src/app/module/core/domain/config.ts
@@ -0,0 +1,11 @@
+export class Config {
+  public static providers: any = {
+    'google': {
+      'clientId': '170472707371-iqab7gbqta7jq9kgt1fbs8ajrh7llcfc.apps.googleusercontent.com'
+    },
+    'facebook': {
+      'clientId': '285797451868355',
+      'apiVersion': '2.8' // like v2.4
+    }
+  };
+}
