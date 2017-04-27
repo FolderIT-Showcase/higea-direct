ca4455ab7525646cb1eb85a83336a2dbc145b14d
Miguel Bustos <mbustos@folderit.net>
2017-04-27 12:32:33 -0300

    Agregado constructor a domain de Persona y seteadas variables como opcionales

--- desktop/src/app/module/core/domain/persona.ts
+++ desktop/src/app/module/core/domain/persona.ts
@@ -1,18 +1,42 @@
 import {User} from './user';
 import {Documento} from './documento';
-import {EstadoCivil} from './enums/estado-civil';
 import {Domicilio} from './domicilio';
 import {Turno} from './turno';
 export class Persona {
-  id: number;
-  apellido: string;
-  fechaNacimiento: Date;
-  genero: string;
-  nombre: string;
-  userAsociado: User;
-  integrantes: Persona[];
-  documento: Documento;
-  estadoCivil: EstadoCivil;
-  domicilio: Domicilio;
-  turno: Turno[];
+    id?: number;
+    principal?: boolean;
+    apellido?: string;
+    fechaNacimiento?: Date;
+    genero?: string;
+    nombre?: string;
+    userAsociado?: User;
+    integrantes?: Persona[];
+    documento?: Documento;
+    estadoCivil?: string;
+    domicilio?: Domicilio;
+    telefono?: string;
+    nacionalidad?: string;
+    turno: Turno[];
+
+    constructor() {
+        this.principal = false;
+        this.documento = {
+            'tipo': undefined,
+            'numero': undefined
+        };
+        this.domicilio = {
+            'pais': {
+                'nombre': undefined,
+                'provincia': {
+                    'nombre': undefined,
+                    'partido': {
+                        'nombre': undefined,
+                        'localidad': {
+                            'nombre': undefined
+                        }
+                    }
+                }
+            }
+        };
+    }
 }
