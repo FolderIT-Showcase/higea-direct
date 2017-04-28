95f9c2a43ad808cff697bdae9575d12335b136bb
Luis Bonsembiante <luisbonsembiante@gmail.com>
2017-04-28 14:41:10 -0300

    enabled turno

--- server/src/main/java/net/folderit/domain/Turno.java
+++ server/src/main/java/net/folderit/domain/Turno.java
@@ -57,7 +57,7 @@ public class Turno implements Serializable {
     private Profesional profesional;
 
     @Column(name = "enabled")
-    private boolean enabled = Boolean.FALSE;
+    private boolean enabled = Boolean.TRUE;
 
     public void finalize() throws Throwable {
 
