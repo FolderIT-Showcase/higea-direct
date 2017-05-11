604e3ec0564e9dece69bdeba7bc175efc5ea9248
Luis Bonsembiante <luisbonsembiante@gmail.com>
2017-05-11 13:25:33 -0300

    alert correccion

--- desktop/src/app/module/core/component/alert/alert.component.ts
+++ desktop/src/app/module/core/component/alert/alert.component.ts
@@ -3,7 +3,8 @@ import {AlertService} from '../../service/alert.service';
 
 @Component({
   selector: 'app-alert',
-  templateUrl: './alert.component.html'
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent implements OnInit {
 
