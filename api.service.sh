2a5e7b8f78cc835310506e29f6da30e50fd53aeb
Fernando Berti <bertilxi@gmail.com>
2017-05-18 17:07:06 -0300

    systemd services

---
+++ api.service.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function startServices {
+
+    /usr/bin/java -jar /opt/api/eureka-server.jar &
+    /usr/bin/java -jar /opt/api/gateway.jar &
+    /usr/bin/java -jar /opt/api/core-server.jar &
+    /usr/bin/java -jar /opt/api/higea-especialidad-microservice.jar &
+    /usr/bin/java -jar /opt/api/higea-profesional-microservice.jar &
+    /usr/bin/java -jar /opt/api/higea-turnos-microservice.jar &
+
+}
+
+
+
+function stopServices {
+
+    /usr/bin/kill -9 `jps -v |grep -w eureka-server.jar|cut -f1 -d " "`
+    /usr/bin/kill -9 `jps -v |grep -w gateway.jar|cut -f1 -d " "`
+    /usr/bin/kill -9 `jps -v |grep -w core-server.jar|cut -f1 -d " "`;\
+    /usr/bin/kill -9 `jps -v |grep -w higea-especialidad-microservice.jar|cut -f1 -d " "`
+    /usr/bin/kill -9 `jps -v |grep -w higea-profesional-microservice.jar|cut -f1 -d " "`
+    /usr/bin/kill -9 `jps -v |grep -w higea-turnos-microservice.jar|cut -f1 -d " "`
+
+}
+
+
+
+
+case "$1"
+in
+    --start) startServices;;
+    --stop) stopServices;;
+    *) help;;
+esac
+
--- jenkins-jobs.sh
+++ jenkins-jobs.sh
@@ -16,50 +16,44 @@ function desktop_dev_mode
 function server_dev_mode
 {
 
+    cd $WORKSPACE/microservices
+
+    mvn clean package -DskipTests
+
     cd $WORKSPACE/microservices/commons
-	mvn clean package -DskipTests
 	sudo cp -f $WORKSPACE/microservices/commons/target/commons-0.0.1-SNAPSHOT.jar $WORKSPACE/microservices/lib
 
     cd $WORKSPACE/microservices/core-server
-    mvn clean package -DskipTests
 	if [ -f /opt/api/core-server.jar ]; then
     		sudo cp -f /opt/api/core-server.jar /opt/api/core-server-last.jar
 	fi
     sudo cp -f $WORKSPACE/microservices/core-server/target/core-server-0.0.1-SNAPSHOT.jar /opt/api/core-server.jar
  
     cd $WORKSPACE/microservices/eureka-server
-    mvn clean package -DskipTests
 	if [ -f /opt/api/eureka-server.jar ]; then
     		sudo cp -f /opt/api/eureka-server.jar /opt/api/eureka-server-last.jar
 	fi
     sudo cp -f $WORKSPACE/microservices/eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar /opt/api/eureka-server.jar
 
     cd $WORKSPACE/microservices/gateway
-    mvn clean package -DskipTests
 	if [ -f /opt/api/gateway.jar ]; then
     		sudo cp -f /opt/api/gateway.jar /opt/api/gateway-last.jar
 	fi
     sudo cp -f $WORKSPACE/microservices/gateway/target/gateway-0.0.1-SNAPSHOT.jar /opt/api/gateway.jar
 
     cd $WORKSPACE/microservices/higea-especialidad-microservice
-	mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false --fail-at-end
-    mvn clean package -DskipTests
 	if [ -f /opt/api/higea-especialidad-microservice.jar ]; then
 	    sudo cp -f /opt/api/higea-especialidad-microservice.jar /opt/api/higea-especialidad-microservice-last.jar
 	fi
-    sudo cp -f$WORKSPACE/microservices/higea-especialidad-microservice/target/higea-especialidad-microservice-0.0.1-SNAPSHOT.jar /opt/api/higea-especialidad-microservice.jar
+    sudo cp -f $WORKSPACE/microservices/higea-especialidad-microservice/target/higea-especialidad-microservice-0.0.1-SNAPSHOT.jar /opt/api/higea-especialidad-microservice.jar
 
     cd $WORKSPACE/microservices/higea-profesional-microservice
-	mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false --fail-at-end
-    mvn clean package -DskipTests
 	if [ -f /opt/api/higea-profesional-microservice.jar ]; then
 	    sudo cp -f /opt/api/higea-profesional-microservice.jar /opt/api/higea-profesional-microservice-last.jar
 	fi
     sudo cp -f $WORKSPACE/microservices/higea-profesional-microservice/target/higea-profesional-microservice-0.0.1-SNAPSHOT.jar /opt/api/higea-profesional-microservice.jar
 
     cd $WORKSPACE/microservices/higea-turnos-microservice
-	mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false --fail-at-end
-    mvn clean package -DskipTests
 	if [ -f /opt/api/higea-turnos-microservice.jar ]; then
 	    sudo cp -f /opt/api/higea-turnos-microservice.jar /opt/api/higea-turnos-microservice-last.jar
 	fi
