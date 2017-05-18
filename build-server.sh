681125dd1468dceb2902abf20820c99322734bbc
Fernando Berti <bertilxi@gmail.com>
2017-05-18 14:15:12 -0300

    modificador script para deploy del server

--- build-server.sh
+++ build-server.sh
@@ -40,6 +40,73 @@ function dev_mode
     
 }
 
+function server_dev_mode
+{
+
+	WORKSPACE = "/home/turnero/turnero-ute"
+
+    cd $WORKSPACE
+
+    # git commands
+    git reset --hard HEAD
+    git checkout master
+    git reset --hard HEAD
+    git pull
+
+    cd $WORKSPACE/microservices/commons
+	mvn clean package -DskipTests
+	sudo cp -f $WORKSPACE/microservices/commons/target/commons-0.0.1-SNAPSHOT.jar $WORKSPACE/microservices/lib
+
+    cd $WORKSPACE/microservices/core-server
+    mvn clean package -DskipTests
+	if [ -f /opt/api/core-server.jar ]; then
+    		sudo cp -f /opt/api/core-server.jar /opt/api/core-server-last.jar
+	fi
+    sudo cp -f $WORKSPACE/microservices/core-server/target/core-server-0.0.1-SNAPSHOT.jar /opt/api/core-server.jar
+ 
+    cd $WORKSPACE/microservices/eureka-server
+    mvn clean package -DskipTests
+	if [ -f /opt/api/eureka-server.jar ]; then
+    		sudo cp -f /opt/api/eureka-server.jar /opt/api/eureka-server-last.jar
+	fi
+    sudo cp -f $WORKSPACE/microservices/eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar /opt/api/eureka-server.jar
+
+    cd $WORKSPACE/microservices/gateway
+    mvn clean package -DskipTests
+	if [ -f /opt/api/gateway.jar ]; then
+    		sudo cp -f /opt/api/gateway.jar /opt/api/gateway-last.jar
+	fi
+    sudo cp -f $WORKSPACE/microservices/gateway/target/gateway-0.0.1-SNAPSHOT.jar /opt/api/gateway.jar
+
+    cd $WORKSPACE/microservices/higea-especialidad-microservice
+	mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false --fail-at-end
+    mvn clean package -DskipTests
+	if [ -f /opt/api/higea-especialidad-microservice.jar ]; then
+	    sudo cp -f /opt/api/higea-especialidad-microservice.jar /opt/api/higea-especialidad-microservice-last.jar
+	fi
+    sudo cp -f$WORKSPACE/microservices/higea-especialidad-microservice/target/higea-especialidad-microservice-0.0.1-SNAPSHOT.jar /opt/api/higea-especialidad-microservice.jar
+
+    cd $WORKSPACE/microservices/higea-profesional-microservice
+	mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false --fail-at-end
+    mvn clean package -DskipTests
+	if [ -f /opt/api/higea-profesional-microservice.jar ]; then
+	    sudo cp -f /opt/api/higea-profesional-microservice.jar /opt/api/higea-profesional-microservice-last.jar
+	fi
+    sudo cp -f $WORKSPACE/microservices/higea-profesional-microservice/target/higea-profesional-microservice-0.0.1-SNAPSHOT.jar /opt/api/higea-profesional-microservice.jar
+
+    cd $WORKSPACE/microservices/higea-turnos-microservice
+	mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false --fail-at-end
+    mvn clean package -DskipTests
+	if [ -f /opt/api/higea-turnos-microservice.jar ]; then
+	    sudo cp -f /opt/api/higea-turnos-microservice.jar /opt/api/higea-turnos-microservice-last.jar
+	fi
+    sudo cp -f $WORKSPACE/microservices/higea-turnos-microservice/target/higea-turnos-microservice-0.0.1-SNAPSHOT.jar /opt/api/higea-turnos-microservice.jar
+
+    # Deploy commands
+    sudo systemctl restart api.service
+    
+}
+
 function help()
 {
     echo 'Usage'
@@ -56,6 +123,6 @@ function help()
 case "$1"
 in
     --prod) prod_mode;;
-    --dev) dev_mode;;
+    --dev) server_dev_mode;;
     *) help;;
 esac
