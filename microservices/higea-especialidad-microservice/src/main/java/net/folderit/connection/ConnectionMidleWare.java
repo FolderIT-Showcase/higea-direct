ea685f4def74a50486eb062714dce63ec6528f25
Fernando Berti <bertilxi@gmail.com>
2017-05-19 17:41:45 -0300

    correccion path microservicio

--- microservices/higea-especialidad-microservice/src/main/java/net/folderit/connection/ConnectionMidleWare.java
+++ microservices/higea-especialidad-microservice/src/main/java/net/folderit/connection/ConnectionMidleWare.java
@@ -17,7 +17,7 @@ public class ConnectionMidleWare {
 
     final String uriLogin = "http://higea.folderit.net/api/login";
     final String uriEspecialidad = "http://higea.folderit.net/api/{cliente}/especialidades";
-    final String uriProfesionales = "http://localhost:36001/{cliente}/profesionales";
+    final String uriProfesionales = "http://localhost:36001/{cliente}/profesional";
     private RestTemplate restTemplate = new RestTemplate();
 
     public ResponseEntity<LoginResultDTO> login() {
