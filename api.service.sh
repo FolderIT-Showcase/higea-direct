336cc4fc544750b092df0c440f24b8859877627b
Fernando Berti <bertilxi@gmail.com>
2017-05-22 11:38:38 -0300

    cambiado services script

--- api.service.sh
+++ api.service.sh
@@ -3,7 +3,7 @@
 function startServices {
 
 #    JAVA_OPTS="-Xms32m -Xmx64m -XX:PermSize=32m -XX:MaxPermSize=64m -XX:+UseG1GC -Djava.security.egd=file:/dev/urandom"
-    JAVA_OPTS="-Xms32m -Xmx64m -XX:+UseG1GC -Djava.security.egd=file:/dev/urandom"
+    JAVA_OPTS="-Xms16m -Xmx32m -XX:+UseG1GC -Djava.security.egd=file:/dev/urandom"
 
     /usr/bin/java $JAVA_OPTS -jar /opt/api/eureka-server.jar < /dev/null &> /dev/null &
     /usr/bin/java $JAVA_OPTS -jar /opt/api/gateway.jar < /dev/null &> /dev/null &
