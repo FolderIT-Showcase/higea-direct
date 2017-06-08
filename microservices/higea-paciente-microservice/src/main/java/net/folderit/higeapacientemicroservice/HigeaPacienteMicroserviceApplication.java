a695c03b071bdce30e46b4e5c50900b104e81a08
Fernando Berti <bertilxi@gmail.com>
2017-06-08 16:00:25 -0300

    paciente re importado

---
+++ microservices/higea-paciente-microservice/src/main/java/net/folderit/higeapacientemicroservice/HigeaPacienteMicroserviceApplication.java
@@ -0,0 +1,31 @@
+package net.folderit.higeapacientemicroservice;
+
+import net.folderit.domain.core.Persona;
+import net.folderit.higeapacientemicroservice.connection.ConnectionMidleWare;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@SpringBootApplication
+public class HigeaPacienteMicroserviceApplication {
+
+    private final ConnectionMidleWare connectionMidleWare;
+
+    @Autowired
+    public HigeaPacienteMicroserviceApplication(ConnectionMidleWare connectionMidleWare) {
+        this.connectionMidleWare = connectionMidleWare;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HigeaPacienteMicroserviceApplication.class, args);
+    }
+
+    @PostMapping("/{cliente}")
+    public ResponseEntity<Persona> save(@PathVariable("cliente") String codigo, @RequestBody Persona persona) {
+        return ResponseEntity.ok(connectionMidleWare.savePaciente(codigo, persona));
+    }
+}
