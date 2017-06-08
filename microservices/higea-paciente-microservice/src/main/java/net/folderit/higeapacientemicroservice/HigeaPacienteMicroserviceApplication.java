c4574e9cefa00aea24338af79ecc802400c1201e
Fernando Berti <bertilxi@gmail.com>
2017-06-08 16:05:49 -0300

    paciente re importado

--- microservices/higea-paciente-microservice/src/main/java/net/folderit/higeapacientemicroservice/HigeaPacienteMicroserviceApplication.java
+++ microservices/higea-paciente-microservice/src/main/java/net/folderit/higeapacientemicroservice/HigeaPacienteMicroserviceApplication.java
@@ -5,12 +5,14 @@ import net.folderit.higeapacientemicroservice.connection.ConnectionMidleWare;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HigeaPacienteMicroserviceApplication {
 
     private final ConnectionMidleWare connectionMidleWare;
