fc85c18c7171363d9b758fc9024c7e16266b955b
Fernando Berti <bertilxi@gmail.com>
2017-06-08 16:21:57 -0300

    paciente re importado

--- microservices/higea-paciente-microservice/src/main/java/net/folderit/higeapacientemicroservice/HigeaPacienteMicroserviceApplication.java
+++ microservices/higea-paciente-microservice/src/main/java/net/folderit/higeapacientemicroservice/HigeaPacienteMicroserviceApplication.java
@@ -10,9 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class HigeaPacienteMicroserviceApplication {
 
     private final ConnectionMidleWare connectionMidleWare;
