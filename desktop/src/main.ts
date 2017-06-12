4716f69cfb3d381bc38df426b7db704e38854308
Fernando Berti <bertilxi@gmail.com>
2017-06-12 12:58:48 -0300

    aot test

--- desktop/src/main.ts
+++ desktop/src/main.ts
@@ -2,9 +2,10 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
+import {COMPILER_PROVIDERS} from '@angular/compiler';
 
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic([...COMPILER_PROVIDERS]).bootstrapModule(AppModule);
+// platformBrowserDynamic().bootstrapModule(AppModule);
